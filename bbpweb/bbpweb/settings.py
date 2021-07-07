9ce6ed7a155e8181493024b0eedb364fa66b484c
emac62 <borderlineboomer@gmail.com>
2021-07-07 10:10:37 -0400

    migration conflict fixes

--- bbpweb/bbpweb/settings.py
+++ bbpweb/bbpweb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['bobbieb.pythonanywhere.com']
+ALLOWED_HOSTS = ["bobbieb.pythonanywhere.com", "127.0.0.1", "bobbieb.ca"]
 
 
 # Application definition
